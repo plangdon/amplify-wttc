4aa2ee8a5244251fa2b308191bf4b89217df4e5f
Paul <Paul@stks-MacBook-Pro.local>
2023-07-04 15:24:11 -0400

    Initial Commit with Mamma Mia

--- src/App.js
+++ src/App.js
@@ -1,22 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <div style={{fontSize: 24, fontWeight: 900, marginBottom: 10}}>
+            Wethersfield Teen Theater Company presents…
+          </div>
+         
+
+          <div>
+            <img src={process.env.PUBLIC_URL + "/000432_hero.jpeg"} />
+          </div>
+
+          <div style={{fontSize: 24, fontWeight: 900, marginBottom: 10}}>
+            Show Dates <br/>
+            Thursday, July 20 - 7:30pm<br/>
+            Friday, July 21 - 7:30pm<br/>
+            Saturday, July 22 - 2:00pm & 7:30pm<br/>
+          </div>
+
+          <div className="row">
+            <div className="column" style={{textAlign: 'left'}}>
+              Tickets on sale July 14<br/>
+              Buy tickets on <a href="https://www.Showtix4u.com" target="_blank">www.Showtix4u.com</a><br/>
+              Or at the door<br/>
+              $12 - adults<br/>
+              $7 - Students and Seniors<br/>
+            </div>
+            <div className="column">
+              Wethersfield High School Auditorium<br/>
+              411 Wolcott Hill Rd<br/>
+              Wethersfield, CT 06109<br/>
+            </div>
+          </div>
+
+          <div>
+            Is presented through special arrangement with Music Theatre International (MTI).  <br/>
+            All authorized performance materials are also supplied by MTI.<br/>
+            www.mtishows.com<br/>
+          </div>
+
+
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
