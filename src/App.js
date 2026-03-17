86873ccb2530eea0ffdae451708b44ee381af1b4
plangdon <plangdon317@gmail.com>
2026-03-17 14:01:20 -0400

    mean girls

--- src/App.js
+++ src/App.js
@@ -11,7 +11,11 @@ function App() {
          
 
           <div>
-            <img src={process.env.PUBLIC_URL + "/sound.jpg"} width={"80%"}/>
+            <img src={process.env.PUBLIC_URL + "/IMG_1839.JPEG"} width={"80%"}/>
+          </div>
+
+          <div style={{fontSize: 16, fontWeight: 900, marginBottom: 10}}>
+            Downoad <a href='{process.env.PUBLIC_URL + "/WTTC_Mean_Girls_Audition_Packet.pdf"}'>WTTC Mean Girls Audition Packet</a>
           </div>
 
         </div>
